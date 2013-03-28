a1ee862309fbd74a0473b377727e9387c4b780b1
Patrick Seidel <psycowithespn1@aim.com>
2013-03-27 23:43:34 -0400

    Game manager

---
+++ src/me/chaseoes/supercraftbrothers/SCBGameManager.java
@@ -0,0 +1,59 @@
+package me.chaseoes.supercraftbrothers;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class SCBGameManager {
+
+    private HashMap<String, SCBGame> games = new HashMap<String, SCBGame>();
+    private HashMap<String, CraftBrother> bros = new HashMap<String, CraftBrother>();
+
+
+    public CraftBrother getCraftBrother(Player player) {
+        if (player == null) return null;
+        return getCraftBrother(player.getName());
+    }
+
+    public CraftBrother getCraftBrother(String player) {
+        if (player == null) return null;
+        if (bros.containsKey(player.toLowerCase())) {
+            return bros.get(player.toLowerCase());
+        }
+        return bros.put(player.toLowerCase(), new CraftBrother(player));
+    }
+
+    public void playerLogout(String player) {
+        if (player == null) return;
+        if (bros.containsKey(player.toLowerCase())) {
+            bros.remove(player.toLowerCase());
+        }
+    }
+
+    public SCBGame getGame(String game) {
+        if (games.containsKey(game.toLowerCase())) {
+            return games.get(game.toLowerCase());
+        }
+        return null;
+    }
+
+    public void addGame(SCBGame game) {
+        if (game == null) return;
+        if (!games.containsKey(game.getName().toLowerCase())) {
+            games.put(game.getName().toLowerCase(), game);
+        }
+    }
+
+    public void removeGame(SCBGame game) {
+        if (game == null) return;
+        removeGame(game.getName());
+    }
+
+    public void removeGame(String game) {
+        if (game == null) return;
+        if (games.containsKey(game.toLowerCase())) {
+            games.remove(game.toLowerCase());
+        }
+    }
+
+}
