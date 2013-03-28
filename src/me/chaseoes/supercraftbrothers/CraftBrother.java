6be33e6356714fbe5d3828e3ac84d533fb436390
chaseoes <chase.oes@gmail.com>
2013-03-27 19:54:27 -0700

    CraftBrother!

---
+++ src/me/chaseoes/supercraftbrothers/CraftBrother.java
@@ -0,0 +1,67 @@
+package me.chaseoes.supercraftbrothers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class CraftBrother {
+    
+    private String name;
+    private int lives;
+    private int kills;
+    private boolean isInLobby;
+    
+    public CraftBrother(String name) {
+        name = this.name;
+        lives = 4;
+        kills = 0;
+    }
+    
+    public Player getPlayer() {
+        return Bukkit.getPlayer(name);
+    }
+    
+    public int getLivesLeft() {
+        return lives;
+    }
+    
+    public void setLivesLeft(int i) {
+        if (i != -1) {
+            lives = i;
+        } else {
+            lives--;
+        }
+    }
+    
+    public int getKills() {
+        return kills;
+    }
+    
+    public void setKills(int i) {
+        if (i != -1) {
+            kills = i;
+        } else {
+            kills--;
+        }
+    }
+    
+    public void setInLobby(boolean b) {
+        isInLobby = b;
+    }
+    
+    public boolean isInLobby() {
+        return isInLobby;
+    }
+    
+    public void getCurrentGame() {
+        
+    }
+    
+    public void getCurrentClass() {
+        
+    }
+    
+    public void setCurrentClass() {
+        
+    }
+
+}
