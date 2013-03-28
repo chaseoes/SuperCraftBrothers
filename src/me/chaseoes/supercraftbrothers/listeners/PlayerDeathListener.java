ad212dea4fda6ac46b7a30d571c895152a18f0c5
chaseoes <chase.oes@gmail.com>
2013-03-27 23:21:58 -0700

    Death messages.

--- src/me/chaseoes/supercraftbrothers/listeners/PlayerDeathListener.java
+++ src/me/chaseoes/supercraftbrothers/listeners/PlayerDeathListener.java
@@ -1,12 +1,19 @@
 package me.chaseoes.supercraftbrothers.listeners;
 
+import me.chaseoes.supercraftbrothers.SCBGameManager;
+import me.chaseoes.supercraftbrothers.SuperCraftBrothers;
+
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlayerDeathListener implements Listener {
 
     public void onPlayerDeath(PlayerDeathEvent event) {
-        // TODO: Formatting code on the death message and possibly the code for
-        // determining lives left.
+        if (SCBGameManager.getInstance().isInGame(event.getEntity().getName())) {
+            event.setDeathMessage(ChatColor.DARK_AQUA + event.getDeathMessage());
+            SuperCraftBrothers.getInstance().getServer().broadcastMessage(ChatColor.DARK_AQUA + event.getEntity().getName() + " has " + SCBGameManager.getInstance().getCraftBrother(event.getEntity()).getLivesLeft() + " lives left.");
+        }
     }
+    
 }
