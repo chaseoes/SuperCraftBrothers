3f3f34bea6010906d3bd1c5c414cbd43f758b4c2
chaseoes <chase.oes@gmail.com>
2013-03-27 23:16:26 -0700

    Refactor package name.

--- src/me/chaseoes/supercraftbrothers/listeners/SCBDeathListener.java
+++ src/me/chaseoes/supercraftbrothers/listeners/SCBDeathListener.java
@@ -1,6 +1,7 @@
 package me.chaseoes.supercraftbrothers.listeners;
 
-import me.chaseoes.supercraftbrothers.event.SCBDeathEvent;
+import me.chaseoes.supercraftbrothers.events.SCBDeathEvent;
+
 import org.bukkit.event.Listener;
 
 public class SCBDeathListener implements Listener {
